c20496d1a13b885a981cf944cd5eb184a2c95743
Randall Rodrigues <randy@mayodesigns.com>
2021-04-05 00:17:51 -0400

    Intersection Observer polyfill.

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"></script>
 	<?php wp_head(); ?>
 </head>
 
