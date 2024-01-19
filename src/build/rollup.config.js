f859d8062e2e66f3e13f50776bdd48a8dda518cd
Randall Rodrigues <randallrodrigues@macbizconsulting.net>
2024-01-18 20:41:01 -0500

    Added missing slick.js ref

--- src/build/rollup.config.js
+++ src/build/rollup.config.js
@@ -58,6 +58,7 @@ module.exports = {
 	input: [
 		path.resolve( __dirname, `../js/${ bsSrcFile }` ),
 		path.resolve( __dirname, '../js/skip-link-focus-fix.js' ),
+		path.resolve(__dirname, '../js/slick.js'),
 		path.resolve( __dirname, '../js/custom-javascript.js' ),
 	],
 	output: [
